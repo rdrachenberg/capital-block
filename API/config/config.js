6356a1006e7a23e6037881add31c33c501bd3eb2
Ryan Drachenberg <tssinvestments@gmail.com>
2020-11-24 14:27:39 -0500

    heroku deploy

--- API/config/config.js
+++ API/config/config.js
@@ -1,14 +1,23 @@
 const env = process.env.NODE_ENV || 'development';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/capital-block';
+const port = process.env.PORT || 8080;
+const secret = process.env.SECRET;
 
 const config = {
     development: {
-        port: process.env.PORT || 8080,
-        dbURL: 'mongodb://localhost:27017/capital-block',
+        port: port,
+        dbURL: dbURL,
+        authCookieName: 'x-auth-token',
+        loggedIn: 'true',
+        secret: secret
+    },
+    production: {
+        port: port,
+        dbURL: dbURL,
         authCookieName: 'x-auth-token',
         loggedIn: 'false',
-        secret: process.env.SECRET
-    },
-    production: {}
+        secret: secret
+    }
 };
 
 module.exports = config[env];
