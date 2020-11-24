18c868af2c36a0763f9750ead092d3bd704d886e
Ryan Drachenberg <tssinvestments@gmail.com>
2020-11-24 14:50:43 -0500

    heroku deployment

--- API/index.js
+++ API/index.js
@@ -15,6 +15,19 @@ dbConnection().then(() => {
         res.status(500).send(err.message);
         console.log('*'.repeat(90))
     });
+    // Join folders
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'API')));
+    // Use folders
+    app.use(express.static('build'));
+    app.use(express.static('public'));
+    app.use(express.static('API'));
+
+      // Handle React routing, return all requests to React app
+        app.get('*', function(req, res) {
+            res.sendFile(path.join(__dirname, '/build', 'index.html'));
+        });
 
     app.listen(config.port, console.log(`🌎 ==> API Server Listening on port ${config.port}! 🌎`))
 
